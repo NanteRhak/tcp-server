f12369fe70bda8f30af22d294c41e60b67d83cbf
TinoRandry <tinorandry@gmail.com>
2026-05-16 04:35:26 +0300

    Partie 5 - Daemon et syslog + README complet

--- include/server.h
+++ include/server.h
@@ -14,7 +14,15 @@
 #define BACKLOG 10
 #define BUFFER_SIZE 1024
 
+<<<<<<< HEAD
 /* Prototypes */
 void handle_client(int connfd, int conn_number);
+=======
+void handle_client_iter(int connfd, int num);
+void daemonize(const char *pidfile);
+char *get_client_ip(struct sockaddr_in *addr, char *buf, int len);
+int write_pid(const char *pidfile);
+int read_pid(const char *pidfile);
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 
 #endif
--- src/server.c
+++ src/server.c
@@ -5,8 +5,8 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <sys/select.h>
 #include <unistd.h>
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 #include <signal.h>
@@ -18,12 +18,30 @@
 >>>>>>> fcd3f5b (Partie 3 - Serveur multi-threade avec pthreads)
 =======
 >>>>>>> fa94fc2 (Partie 4 - Multiplexage IO avec select)
+=======
+#include <signal.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <syslog.h>
+#include <arpa/inet.h>
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 #include "server.h"
+
+static int *connexions_actives;
+
+static void sigchld_handler(int sig) {
+    (void)sig;
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        (*connexions_actives)--;
+}
 
 int main(void) {
     int listenfd, connfd;
-    struct sockaddr_in srv;
+    struct sockaddr_in srv, cli;
+    socklen_t clilen = sizeof(cli);
+    int conn_num = 0;
     int opt = 1;
+<<<<<<< HEAD
 <<<<<<< HEAD
     int connection_count = 0;
 =======
@@ -33,6 +51,23 @@ int main(void) {
 
     for (i = 0; i < FD_SETSIZE; i++) clients[i] = -1;
 >>>>>>> fa94fc2 (Partie 4 - Multiplexage IO avec select)
+=======
+
+    openlog("myserverd", LOG_PID | LOG_CONS, LOG_DAEMON);
+
+    daemonize("/tmp/myserverd.pid");
+
+    connexions_actives = mmap(NULL, sizeof(int),
+        PROT_READ | PROT_WRITE,
+        MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    *connexions_actives = 0;
+
+    struct sigaction sa;
+    sa.sa_handler = sigchld_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+    sigaction(SIGCHLD, &sa, NULL);
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 
 <<<<<<< HEAD
 <<<<<<< HEAD
@@ -56,8 +91,12 @@ int main(void) {
 =======
 >>>>>>> fcd3f5b (Partie 3 - Serveur multi-threade avec pthreads)
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
+<<<<<<< HEAD
     if (listenfd < 0) { perror("socket"); exit(1); }
 >>>>>>> 60622f8 (Partie 2 - Serveur concurrent avec fork)
+=======
+    if (listenfd < 0) { syslog(LOG_ERR, "socket() échoué : %m"); exit(1); }
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 
     // 2. Configuration SO_REUSEADDR 
     if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
@@ -71,13 +110,19 @@ int main(void) {
     srv.sin_addr.s_addr = INADDR_ANY;
     srv.sin_port = htons(PORT);
 
+<<<<<<< HEAD
     if (bind(listenfd, (struct sockaddr *)&srv, sizeof(srv)) < 0) {
         perror("bind");
         exit(EXIT_FAILURE);
+=======
+    if (bind(listenfd, (struct sockaddr*)&srv, sizeof(srv)) < 0) {
+        syslog(LOG_ERR, "bind() échoué : %m"); exit(1);
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
     }
 
     // 4. Mise en écoute
     if (listen(listenfd, BACKLOG) < 0) {
+<<<<<<< HEAD
         perror("listen");
         exit(EXIT_FAILURE);
     }
@@ -99,9 +144,16 @@ int main(void) {
 
     printf("Serveur select démarré sur le port %d\n", PORT);
 >>>>>>> fa94fc2 (Partie 4 - Multiplexage IO avec select)
+=======
+        syslog(LOG_ERR, "listen() échoué : %m"); exit(1);
+    }
+
+    syslog(LOG_INFO, "Daemon démarré sur le port %d", PORT);
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 
     // 5. Boucle principale 
     while (1) {
+<<<<<<< HEAD
 <<<<<<< HEAD
         connfd = accept(listenfd, NULL, NULL);
 <<<<<<< HEAD
@@ -146,23 +198,19 @@ int main(void) {
         pthread_mutex_unlock(&mutex);
 =======
 >>>>>>> fa94fc2 (Partie 4 - Multiplexage IO avec select)
+=======
+        connfd = accept(listenfd, (struct sockaddr*)&cli, &clilen);
+        if (connfd < 0) { syslog(LOG_WARNING, "accept() échoué : %m"); continue; }
 
-        for (i = 0; i <= maxi; i++) {
-            if (clients[i] == -1) continue;
-            if (FD_ISSET(clients[i], &rset)) {
-                char buf[BUF_SIZE];
-                char response[BUF_SIZE + 64];
-                ssize_t n = read(clients[i], buf, sizeof(buf) - 1);
-                if (n <= 0) {
-                    close(clients[i]);
-                    FD_CLR(clients[i], &allset);
-                    clients[i] = -1;
-                } else {
-                    buf[n] = '\0';
-                    snprintf(response, sizeof(response), "Echo : %s", buf);
-                    write(clients[i], response, strlen(response));
-                }
+        char ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &cli.sin_addr, ip, sizeof(ip));
+        syslog(LOG_INFO, "Connexion acceptée de %s:%d", ip, ntohs(cli.sin_port));
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
 
+        pid_t pid = fork();
+        if (pid < 0) { syslog(LOG_ERR, "fork() échoué : %m"); close(connfd); continue; }
+
+<<<<<<< HEAD
 <<<<<<< HEAD
         pthread_t tid;
         pthread_create(&tid, NULL, handle_client_thread, fd_copy);
@@ -181,5 +229,19 @@ int main(void) {
     }
 
     close(listenfd);
+=======
+        if (pid == 0) {
+            close(listenfd);
+            (*connexions_actives)++;
+            handle_client_iter(connfd, ++conn_num);
+            syslog(LOG_INFO, "Client traité (fd=%d)", connfd);
+            close(connfd);
+            exit(0);
+        }
+        close(connfd);
+    }
+
+    closelog();
+>>>>>>> 1619af4 (Partie 5 - Daemon et syslog + README complet)
     return 0;
 }
